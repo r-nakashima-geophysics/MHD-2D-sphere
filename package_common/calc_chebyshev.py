ef590330aea972bb5833431454272380363a52d0
Nakashima Ryosuke <r.nakashima.a10@panda.kobe-u.ac.jp>
2025-08-06 19:01:20 +0900

    feat: improve a module for Chebyshev polynomials

--- package_common/calc_chebyshev.py
+++ package_common/calc_chebyshev.py
@@ -2,42 +2,34 @@
 polynomials"""
 
 import cmath
-import math
 
 from numba import njit
-
-from package_common.common_types import TypeVar
-
-T = TypeVar("T", float, complex)
 
 
 @njit
 def chebyshev(n_degree: int,
-              s_pos: T) -> T:
+              s_pos: complex) -> complex:
     """Calculate the value of a Chebyshev polynomial at a point
 
     Parameters
     ----------
     n_degree : int
         The degree of the Chebyshev polynomial
-    s_pos : T
+    s_pos : complex
         The position of the point
 
     Returns
     ----------
-    value_chebyshev : T
+    complex
         The value of the Chebyshev polynomial at the point
     """
-
-    if isinstance(s_pos, float):
-        return math.cos(n_degree * math.acos(s_pos))
 
     return cmath.cos(n_degree * cmath.acos(s_pos))
 
 
 @njit
 def chebyshev_d(n_degree: int,
-                s_pos: T) -> T:
+                s_pos: complex) -> complex:
     """Calculate the value of the first derivative of a Chebyshev
     polynomial at a point
 
@@ -45,36 +37,23 @@ def chebyshev_d(n_degree: int,
     ----------
     n_degree : int
         The degree of the Chebyshev polynomial
-    s_pos : T
+    s_pos : complex
         The position of the point
 
     Returns
     ----------
-    value_chebyshev_d : T
+    complex
         The value of the first derivative of the Chebyshev polynomial at
         the point
     """
 
-    value_chebyshev_d: T = 0.0
-    if n_degree == 0:
-        return value_chebyshev_d
-
-    if n_degree % 2 == 0:
-        for i_n in range(1, n_degree, 2):
-            value_chebyshev_d += chebyshev(i_n, s_pos)
-    else:
-        for i_n in range(2, n_degree, 2):
-            value_chebyshev_d += chebyshev(i_n, s_pos)
-        value_chebyshev_d += chebyshev(0, s_pos) / 2
-
-    value_chebyshev_d *= 2 * n_degree
-
-    return value_chebyshev_d
+    t: complex = cmath.acos(s_pos)
+    return n_degree * cmath.sin(n_degree*t) / cmath.sin(t)
 
 
 @njit
 def chebyshev_d2(n_degree: int,
-                 s_pos: T) -> T:
+                 s_pos: complex) -> complex:
     """Calculate the value of the second derivative of a Chebyshev
     polynomial at a point
 
@@ -82,39 +61,25 @@ def chebyshev_d2(n_degree: int,
     ----------
     n_degree : int
         The degree of the Chebyshev polynomial
-    s_pos : T
+    s_pos : complex
         The position of the point
 
     Returns
     ----------
-    value_chebyshev_d2 : T
+    complex
         The value of the second derivative of the Chebyshev polynomial
         at the point
     """
 
-    value_chebyshev_d2: T = 0.0
-    if 0 <= n_degree <= 1:
-        return value_chebyshev_d2
-
-    if n_degree % 2 == 0:
-        for i_n in range(2, n_degree-1, 2):
-            value_chebyshev_d2 += ((n_degree**2)-(i_n**2)) \
-                * chebyshev(i_n, s_pos)
-        value_chebyshev_d2 \
-            += (n_degree**2) * chebyshev(0, s_pos) / 2
-    else:
-        for i_n in range(1, n_degree-1, 2):
-            value_chebyshev_d2 += ((n_degree**2)-(i_n**2)) \
-                * chebyshev(i_n, s_pos)
-
-    value_chebyshev_d2 *= n_degree
-
-    return value_chebyshev_d2
+    t: complex = cmath.acos(s_pos)
+    return (-n_degree**2 * cmath.cos(n_degree*t)
+            + chebyshev_d(n_degree, s_pos) * cmath.cos(t)
+            ) / (cmath.sin(t)**2)
 
 
 @njit
 def chebyshev_d3(n_degree: int,
-                 s_pos: T) -> T:
+                 s_pos: complex) -> complex:
     """Calculate the value of the third derivative of a Chebyshev
     polynomial at a point
 
@@ -122,35 +87,18 @@ def chebyshev_d3(n_degree: int,
     ----------
     n_degree : int
         The degree of the Chebyshev polynomial
-    s_pos : T
+    s_pos : complex
         The position of the point
 
     Returns
     ----------
-    value_chebyshev_d3 : T
+    complex
         The value of the third derivative of the Chebyshev polynomial at
         the point
 
     """
 
-    value_chebyshev_d3: T = 0.0
-    if 0 <= n_degree <= 2:
-        return value_chebyshev_d3
-
-    if n_degree % 2 == 0:
-        for i_n in range(1, n_degree-2, 2):
-            value_chebyshev_d3 += (((n_degree+1)**2)-(i_n**2)) \
-                * (((n_degree-1)**2)-(i_n**2)) \
-                * chebyshev(i_n, s_pos)
-    else:
-        for i_n in range(2, n_degree-2, 2):
-            value_chebyshev_d3 += (((n_degree+1)**2)-(i_n**2)) \
-                * (((n_degree-1)**2)-(i_n**2)) \
-                * chebyshev(i_n, s_pos)
-        value_chebyshev_d3 \
-            += ((n_degree+1)**2) * ((n_degree-1)**2) \
-            * chebyshev(0, s_pos) / 2
-
-    value_chebyshev_d3 *= n_degree / 4
-
-    return value_chebyshev_d3
+    t: complex = cmath.acos(s_pos)
+    return ((1-n_degree**2) * chebyshev_d(n_degree, s_pos)
+            + chebyshev_d2(n_degree, s_pos) * 3 * cmath.cos(t)
+            ) / (cmath.sin(t)**2)
