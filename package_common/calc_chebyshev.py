246c7e9bdd6caa4dd768eaff13c1412b76300ef0
Nakashima Ryosuke <r.nakashima.a10@panda.kobe-u.ac.jp>
2025-08-08 22:52:51 +0900

    feat: use numpy instead of cmath

--- package_common/calc_chebyshev.py
+++ package_common/calc_chebyshev.py
@@ -1,104 +1,103 @@
 """A Python module to calculate values related to Chebyshev
-polynomials"""
+polynomials."""
 
-import cmath
-
+import numpy as np
 from numba import njit
 
 
 @njit
 def chebyshev(n_degree: int,
               s_pos: complex) -> complex:
-    """Calculate the value of a Chebyshev polynomial at a point
+    """Calculate the value of a Chebyshev polynomial at a given point.
 
     Parameters
     ----------
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of the Chebyshev polynomial.
     s_pos : complex
-        The position of the point
+        The position of the point.
 
     Returns
     ----------
     complex
-        The value of the Chebyshev polynomial at the point
+        The value of the Chebyshev polynomial at the point.
     """
 
-    return cmath.cos(n_degree * cmath.acos(s_pos))
+    return np.cos(n_degree * np.acos(s_pos))
 
 
 @njit
 def chebyshev_d(n_degree: int,
                 s_pos: complex) -> complex:
     """Calculate the value of the first derivative of a Chebyshev
-    polynomial at a point
+    polynomial at a given point.
 
     Parameters
     ----------
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of the Chebyshev polynomial.
     s_pos : complex
-        The position of the point
+        The position of the point.
 
     Returns
     ----------
     complex
         The value of the first derivative of the Chebyshev polynomial at
-        the point
+        the point.
     """
 
-    t: complex = cmath.acos(s_pos)
-    return n_degree * cmath.sin(n_degree*t) / cmath.sin(t)
+    t: complex = np.acos(s_pos)
+    return n_degree * np.sin(n_degree*t) / np.sin(t)
 
 
 @njit
 def chebyshev_d2(n_degree: int,
                  s_pos: complex) -> complex:
     """Calculate the value of the second derivative of a Chebyshev
-    polynomial at a point
+    polynomial at a given point.
 
     Parameters
     ----------
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of the Chebyshev polynomial.
     s_pos : complex
-        The position of the point
+        The position of the point.
 
     Returns
     ----------
     complex
         The value of the second derivative of the Chebyshev polynomial
-        at the point
+        at the point.
     """
 
-    t: complex = cmath.acos(s_pos)
-    return (-n_degree**2 * cmath.cos(n_degree*t)
-            + chebyshev_d(n_degree, s_pos) * cmath.cos(t)
-            ) / (cmath.sin(t)**2)
+    t: complex = np.acos(s_pos)
+    return (-n_degree**2 * np.cos(n_degree*t)
+            + chebyshev_d(n_degree, s_pos) * np.cos(t)
+            ) / (np.sin(t)**2)
 
 
 @njit
 def chebyshev_d3(n_degree: int,
                  s_pos: complex) -> complex:
     """Calculate the value of the third derivative of a Chebyshev
-    polynomial at a point
+    polynomial at a given point.
 
     Parameters
     ----------
     n_degree : int
-        The degree of the Chebyshev polynomial
+        The degree of the Chebyshev polynomial.
     s_pos : complex
-        The position of the point
+        The position of the point.
 
     Returns
     ----------
     complex
         The value of the third derivative of the Chebyshev polynomial at
-        the point
+        the point.
 
     """
 
-    t: complex = cmath.acos(s_pos)
+    t: complex = np.acos(s_pos)
     return ((1-n_degree**2) * chebyshev_d(n_degree, s_pos)
-            + chebyshev_d2(n_degree, s_pos) * 3 * cmath.cos(t)
-            ) / (cmath.sin(t)**2)
+            + chebyshev_d2(n_degree, s_pos) * 3 * np.cos(t)
+            ) / (np.sin(t)**2)
