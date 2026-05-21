360c9ecda0de3b0f63cd0ea7770c1cccaa26275b
Nakashima Ryosuke <r.nakashima.a10@panda.kobe-u.ac.jp>
2026-05-21 20:13:14 +0900

    feat: modify a function

--- package_mhd2dsphere/init_background_b.py
+++ package_mhd2dsphere/init_background_b.py
@@ -295,22 +295,24 @@ def b_sin2cos(switch_theta: str = 'mu') -> BackgroundField:
     sys.exit(1)
 
 
-def b_malkussc(strength_sincos: float, switch_theta: str = 'mu') \
-        -> BackgroundField:
+def b_malsincos(ratio_sincos2malkus:
+                float, switch_theta: str = 'mu') -> BackgroundField:
     """Construct an instance of the BackgroundField class for B =
-    const + cos(theta).
+    const * (1 + ratio_sincos2malkus * cos(theta)).
 
     Parameters
     ----------
-    strength_sincos : float
-        The strength of the sincos part of the background field.
+    ratio_sincos2malkus : float
+        The ratio of the sincos part to the Malkus part of the background
+        field.
     switch_theta : str, default 'mu'
         The string to switch whether either mu (= cos(theta)) or theta is used.
 
     Returns
     -------
     BackgroundField
-        The instance of the BackgroundField class for B = const + cos(theta).
+        The instance of the BackgroundField class for B = const * (1 +
+        ratio_sincos2malkus * cos(theta)).
 
     Warnings
     --------
@@ -320,48 +322,52 @@ def b_malkussc(strength_sincos: float, switch_theta: str = 'mu') \
     Examples
     --------
     >>> from package_mhd2dsphere import init_background_b
-    >>> b_malkussc = init_background_b.b_malkussc(1.0, 'mu')
-    >>> b_malkussc = init_background_b.b_malkussc(1.0, 'theta')
+    >>> b_malsincos = init_background_b.b_malsincos(1.0, 'mu')
+    >>> b_malsincos = init_background_b.b_malsincos(1.0, 'theta')
     """
 
     logger: DefaultLogger = create_function_name_logger()
 
-    name: str = f'malkussc{strength_sincos:.1f}'
-    tex: str = r'$B_{0\phi}=B_0\sin\theta(1+' \
-        + str(strength_sincos) + r'\cos\theta)$'
+    name: str = f'malsincos{ratio_sincos2malkus:.2f}'
+
+    malkus: float = 1 / (1+ratio_sincos2malkus)
+    sincos: float = ratio_sincos2malkus / (1+ratio_sincos2malkus)
+
+    tex: str = r'$B_{0\phi}=B_0\sin\theta(' \
+        + str(malkus) + r' + ' + str(sincos) + r'\cos\theta)$'
 
     if switch_theta == 'mu':
-        def b_malkussc_mu(mu_complex: complex) -> complex:
-            return 1 + strength_sincos * mu_complex
+        def b_malsincos_mu(mu_complex: complex) -> complex:
+            return malkus + sincos * mu_complex
 
-        def b_malkussc_d_mu(mu_complex: complex) -> complex:
+        def b_malsincos_d_mu(mu_complex: complex) -> complex:
             _ = mu_complex
-            return strength_sincos
+            return sincos
 
-        def b_malkussc_d2_mu(mu_complex: complex) -> complex:
+        def b_malsincos_d2_mu(mu_complex: complex) -> complex:
             _ = mu_complex
             return 0
 
         return BackgroundField(name,
-                               value=b_malkussc_mu,
-                               value_d=b_malkussc_d_mu,
-                               value_d2=b_malkussc_d2_mu,
+                               value=b_malsincos_mu,
+                               value_d=b_malsincos_d_mu,
+                               value_d2=b_malsincos_d2_mu,
                                tex=tex)
 
     if switch_theta == 'theta':
-        def b_malkussc_theta(theta_complex: complex) -> complex:
-            return 1 + strength_sincos * np.cos(theta_complex)
+        def b_malsincos_theta(theta_complex: complex) -> complex:
+            return malkus + sincos * np.cos(theta_complex)
 
-        def b_malkussc_d_theta(theta_complex: complex) -> complex:
-            return -strength_sincos * np.sin(theta_complex)
+        def b_malsincos_d_theta(theta_complex: complex) -> complex:
+            return -sincos * np.sin(theta_complex)
 
-        def b_malkussc_d2_theta(theta_complex: complex) -> complex:
-            return -strength_sincos * np.cos(theta_complex)
+        def b_malsincos_d2_theta(theta_complex: complex) -> complex:
+            return -sincos * np.cos(theta_complex)
 
         return BackgroundField(name,
-                               value=b_malkussc_theta,
-                               value_d=b_malkussc_d_theta,
-                               value_d2=b_malkussc_d2_theta,
+                               value=b_malsincos_theta,
+                               value_d=b_malsincos_d_theta,
+                               value_d2=b_malsincos_d2_theta,
                                tex=tex)
 
     logger.error('Invalid argument')
