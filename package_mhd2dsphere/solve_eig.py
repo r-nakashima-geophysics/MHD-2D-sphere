08af28d784dc41abd2ca01f557d7f83b566e9a06
Nakashima Ryosuke <r.nakashima.a10@panda.kobe-u.ac.jp>
2026-07-23 21:33:22 +0900

    docs: improve type hints

--- package_mhd2dsphere/solve_eig.py
+++ package_mhd2dsphere/solve_eig.py
@@ -84,14 +84,14 @@ def prepare_chebyshev_gauss_quad(
             m_order, n_degree, s_pos, mu_complex=mu_complex)
 
     def _weight_psm_1(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        b_mu = bg_field_b.r_value(mu)
+        mu: float = mu_complex.r_value(s_pos)
+        b_mu: float = bg_field_b.r_value(mu)
         return (-1) / (4 * b_mu)
 
     def _weight_psm_2(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        b_mu = bg_field_b.r_value(mu)
-        u_shear_mu = (
+        mu: float = mu_complex.r_value(s_pos)
+        b_mu: float = bg_field_b.r_value(mu)
+        u_shear_mu: float = (
             bg_field_u.r_value_d2(mu) * (1-(mu**2))
             - 4 * mu * bg_field_u.r_value_d(mu)
             - 2 * bg_field_u.r_value(mu)
@@ -99,19 +99,19 @@ def prepare_chebyshev_gauss_quad(
         return (-1) * (rossby*u_shear_mu-1) / (4 * (b_mu**2))
 
     def _weight_pse_u1(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        u_mu = bg_field_u.r_value(mu)
+        mu: float = mu_complex.r_value(s_pos)
+        u_mu: float = bg_field_u.r_value(mu)
         return 4 * rossby * u_mu * _weight_psm_1(s_pos)
 
     def _weight_pse_u2(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        u_mu = bg_field_u.r_value(mu)
+        mu: float = mu_complex.r_value(s_pos)
+        u_mu: float = bg_field_u.r_value(mu)
         return 4 * rossby * u_mu * _weight_psm_2(s_pos)
 
     def _weight_pse_b(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        b_mu = bg_field_b.r_value(mu)
-        b_shear_mu = (
+        mu: float = mu_complex.r_value(s_pos)
+        b_mu: float = bg_field_b.r_value(mu)
+        b_shear_mu: float = (
             bg_field_b.r_value_d2(mu) * (1-(mu**2))
             - 4 * mu * bg_field_b.r_value_d(mu)
             - 2 * bg_field_b.r_value(mu)
@@ -119,8 +119,8 @@ def prepare_chebyshev_gauss_quad(
         return b_shear_mu / b_mu
 
     def _weight_psm_hd(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        u_shear_mu = (
+        mu: float = mu_complex.r_value(s_pos)
+        u_shear_mu: float = (
             bg_field_u.r_value_d2(mu) * (1-(mu**2))
             - 4 * mu * bg_field_u.r_value_d(mu)
             - 2 * bg_field_u.r_value(mu)
@@ -128,8 +128,8 @@ def prepare_chebyshev_gauss_quad(
         return 1 / (4 * (rossby*u_shear_mu-1))
 
     def _weight_pse_hd(s_pos: float) -> float:
-        mu = mu_complex.r_value(s_pos)
-        u_mu = bg_field_u.r_value(mu)
+        mu: float = mu_complex.r_value(s_pos)
+        u_mu: float = bg_field_u.r_value(mu)
         return 4 * rossby * u_mu * _weight_psm_hd(s_pos)
 
     quad: DictChebyshevGaussQuad = {
